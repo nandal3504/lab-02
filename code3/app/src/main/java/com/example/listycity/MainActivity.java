ef4053b14a1ddd48fce72cee604adec2fdce3ec4
unknown <nandal@ualberta.ca>
2026-01-15 18:16:39 -0700

    Lab-02

--- code3/app/src/main/java/com/example/listycity/MainActivity.java
+++ code3/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        cityList = findViewById(R.id.city_list);
+        String []cities= {"Edmonton", "Vancouver","Moscow", "Sydney", "Berlin","Vienna","Tokyo", "Beijing", "Osaka", "New Delhi" };
+
+        dataList = new ArrayList<>();;
+        dataList.addAll(Arrays.asList(cities));
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
     }
 }
